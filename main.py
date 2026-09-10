bb14043a43b54da21bf6e985325d206f169ffa91
Denis Rudenko <dr144371@gmail.com>
2026-09-10 16:17:33 +0300

    Add input values validation

--- main.py
+++ main.py
@@ -25,6 +25,12 @@ if len(sys.argv) - 1 == 1:
     a = inputWithValidation("Введите A: ")
     b = inputWithValidation("Введите B: ")
     c = inputWithValidation("Введите C: ")
+
+    if abs(a) > 10000 or abs(b) > 10000 or abs(c) > 10000:
+        print("ОШИБКА: значение вне допустимого диапазона")
+        sys.exit(1)
+
+
 elif len(sys.argv) - 1 == 7:
     if sys.argv[2] != "-a" or sys.argv[4] != "-b" or sys.argv[6] != "-c":
         print("ОШИБКА: неизвестный параметр")
