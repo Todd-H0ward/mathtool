a21fa7ce5a5ce8a3b06d26b9a100562a4ef16f80
Denis Rudenko <dr144371@gmail.com>
2026-09-10 16:16:14 +0300

    Add input validation

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import sys
 
+# ------ HELPERS --------
+
+def inputWithValidation(message):
+    try:
+        num = int(input(message))
+    except:
+        print("ОШИБКА: коэффициент не является целым числом")
+        sys.exit(1)
+
+    return num
+
+# -------- MAIN ---------
+
 if len(sys.argv) == 1 or sys.argv[1] == "--help":
     print("Справка") #TODO
     sys.exit(0)
 
 if sys.argv[1] != "solve":
-    print("Ошибка: неизвестная команда")
+    print("ОШИБКА: неизвестная команда")
     sys.exit(1)
 
 if len(sys.argv) - 1 == 1:
-    a = input("Введите A: ")
-    b = input("Введите B: ")
-    c = input("Введите C: ")
+    a = inputWithValidation("Введите A: ")
+    b = inputWithValidation("Введите B: ")
+    c = inputWithValidation("Введите C: ")
 elif len(sys.argv) - 1 == 7:
     if sys.argv[2] != "-a" or sys.argv[4] != "-b" or sys.argv[6] != "-c":
-        print("Ошибка: неизвестный параметр")
+        print("ОШИБКА: неизвестный параметр")
         sys.exit(1)
 
     a = sys.argv[3]
     b = sys.argv[5]
     c = sys.argv[7]
 else:
-    print("Ошибка: неизвестный набор параметров")
+    print("ОШИБКА: неизвестный набор параметров")
     sys.exit(1)
